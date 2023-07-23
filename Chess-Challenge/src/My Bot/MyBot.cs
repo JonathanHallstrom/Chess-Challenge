29d6e6f841d21a6ed1a6aab1ec2a09499eb5b23d
Maxwell Penders <max.penders@gmail.com>
2023-07-23 17:38:56 -0400

    Added basic reasoning

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,25 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections;
+using ChessChallenge.API;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        ArrayList moveorder = new ArrayList();
+        foreach (Move mv in moves)
+            {
+                if (mv.IsCapture || mv.IsPromotion)
+                {
+                    moveorder.Add(mv);
+                }
+            }
+
+        if (moveorder.Count > 0)
+        {
+            return (Move) moveorder[0];
+        } 
+            return moves[0];
     }
 }
