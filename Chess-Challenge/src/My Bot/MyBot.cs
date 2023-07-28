75dc7fc0896ecea67c91d605ccb7ef2a86296896
JonathanHallstrom <lmj.hallstrom@gmail.com>
2023-07-28 17:18:39 +0200

    small rewrite to save tokens

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,18 +6,13 @@ using Microsoft.CodeAnalysis;
 
 public sealed class MyBot : IChessBot
 {
+    // save tokens
+    Move[] legalMoves;
+    int eval;
+    
     int[] pieceValues = { 0, 100, 305, 333, 563, 950, 10000 };
-
-    int value(Piece p)
-    {
-        return pieceValues[(int)p.PieceType];
-    }
-
-    int value(PieceType p)
-    {
-        return pieceValues[(int)p];
-    }
-
+    int CHECKMATE = (int)1e9;
+        
     const ulong lookupTableSize = 8 << 20;
 
     // store hashes for identity comparison, collisions should be reasonably rare
@@ -25,16 +20,32 @@ public sealed class MyBot : IChessBot
     // store evaluations (goes with `hashes`)
     static int[] values = new int[lookupTableSize];
 
-
-    Move chosenMove;
-
-    int maxDepth;
-
-    bool endGame;
-
-    int negaMax(Board board, int depth, int alpha, int beta, int color, int bestEval = int.MinValue)
+    int Eval(Board board)
     {
-        ulong hash = board.ZobristKey ^ (ulong)(color + 1);
+        eval = 0;
+        //  being checkmated is bad
+        if (board.IsInCheckmate())
+            return -1000_000_000;
+
+        if (board.IsInCheck())
+            // has to be at least 100, or black won't checkmate
+            eval += board.IsWhiteToMove ? -100 : 100;
+
+        // sum up the pieces from whites perspective
+        for (int i = 1; i < 7; ++i)
+            eval += board.GetPieceList((PieceType)i, true).Count * pieceValues[i];
+
+        // sum up the pieces from blacks perspective
+        for (int i = 1; i < 7; ++i)
+            eval -= board.GetPieceList((PieceType)i, false).Count * pieceValues[i];
+
+        return (board.IsWhiteToMove ? 1 : -1) * eval;
+
+    }
+
+    int negaMax(Board board, int depth, int alpha, int beta, int bestEval = int.MinValue)
+    {
+        ulong hash = board.ZobristKey;
 
         if (hashes[hash % lookupTableSize] == hash)
             return values[hash % lookupTableSize];
@@ -42,45 +53,21 @@ public sealed class MyBot : IChessBot
         if (board.IsDraw())
             return 0;
 
-        Move[] legalMoves = board.GetLegalMoves();
-
+        legalMoves = board.GetLegalMoves();
         // if we finished the search, evaluate the position
         if (depth == 0 || legalMoves.Length == 0)
-        {
-            int eval = 0;
-            //  being checkmated is bad
-            if (board.IsInCheckmate())
-                return -1000_000_000;
-                    
-            if (board.IsInCheck())
-                // has to be at least 100, or black won't checkmate
-                eval += board.IsWhiteToMove ? -100 : 100;
-
-            // sum up the pieces from whites perspective
-            for (int i = 1; i < 7; ++i)
-                eval += board.GetPieceList((PieceType)i, true).Count * pieceValues[i];
-
-            // sum up the pieces from blacks perspective
-            for (int i = 1; i < 7; ++i)
-                eval -= board.GetPieceList((PieceType)i, false).Count * pieceValues[i];
-
-            // if we're white, color will be 1 otherwise -1
-            return color * eval;
-        }
+            return Eval(board);
 
         //int bestEval = -Int32.MaxValue;
         foreach (Move move in legalMoves)
         {
             board.MakeMove(move);
             // decay added for further-out moves to stop prioritizing longer checkmates
-            int evaluation = (int) (0.99 * -negaMax(board, depth - 1, -beta, -alpha, -color));
+            int evaluation = (int)(0.99 * -negaMax(board, depth - 1, -beta, -alpha));
             board.UndoMove(move);
 
             if (evaluation > bestEval)
-            {
                 bestEval = evaluation;
-                if (depth == maxDepth) chosenMove = move;
-            }
             alpha = Math.Max(alpha, bestEval);
             if (alpha >= beta) break;
         }
@@ -94,30 +81,24 @@ public sealed class MyBot : IChessBot
     {
         Array.Clear(hashes);
         Array.Clear(values);
-        int enemyPieceCount = 0;
-        int myPieceCount = 0;
+        
+        legalMoves = board.GetLegalMoves();
+        Move chosenMove = legalMoves[0];
+        int bestEval = -CHECKMATE;;
 
-        foreach (var pieceList in board.GetAllPieceLists())
+        foreach (Move move in legalMoves)
         {
-            if (pieceList.IsWhitePieceList == board.IsWhiteToMove)
-                myPieceCount += pieceList.Count;
-            else
-                enemyPieceCount += pieceList.Count;
+            board.MakeMove(move);
+            eval = -negaMax(board, 5 - 1, -CHECKMATE, CHECKMATE);
+            board.UndoMove(move);
+            if (eval > bestEval)
+            {
+                chosenMove = move;
+                bestEval = eval;
+            }
+            
         }
         
-        chosenMove = board.GetLegalMoves()[0];
-
-        if (enemyPieceCount * 2 <= myPieceCount)
-            endGame = true;
-        else
-            endGame = false;
-        
-        // so far this seems to be fine
-        maxDepth = endGame ? 8 : 5;
-
-        negaMax(board, maxDepth, -Int32.MaxValue, Int32.MaxValue, board.IsWhiteToMove ? 1 : -1);
-        
-
         return chosenMove;
     }
 }
--- Chess-Challenge/src/My Bot/MyBotOld.cs
+++ Chess-Challenge/src/My Bot/MyBotOld.cs
@@ -2,8 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using ChessChallenge.API;
+using Microsoft.CodeAnalysis;
 
-public class MyBotOld : IChessBot
+public sealed class MyBotOld : IChessBot
 {
     int[] pieceValues = { 0, 100, 305, 333, 563, 950, 10000 };
 
@@ -17,84 +18,106 @@ public class MyBotOld : IChessBot
         return pieceValues[(int)p];
     }
 
-    int eval(Board board, Move move)
+    const ulong lookupTableSize = 8 << 20;
+
+    // store hashes for identity comparison, collisions should be reasonably rare
+    static ulong[] hashes = new ulong[lookupTableSize];
+    // store evaluations (goes with `hashes`)
+    static int[] values = new int[lookupTableSize];
+
+
+    Move chosenMove;
+
+    int maxDepth;
+
+    bool endGame;
+
+    int negaMax(Board board, int depth, int alpha, int beta, int color, int bestEval = int.MinValue)
     {
-        if (isCheckmate(board, move))
-            return int.MaxValue;
+        ulong hash = board.ZobristKey ^ (ulong)(color + 1);
 
-        int res = 0;
-        // if (isCheck(board, move))
-        //     res += 50;
+        if (hashes[hash % lookupTableSize] == hash)
+            return values[hash % lookupTableSize];
 
-        int myPieceValue = value(move.MovePieceType);
-        
-        Piece targetedPiece = board.GetPiece(move.TargetSquare);
-        if (move.IsCapture)
-             // value of opponents piece  value of our piece (to prefer capturing with less valuable pieces
-            res += value(targetedPiece) - myPieceValue / 4;
+        if (board.IsDraw())
+            return 0;
 
-        if (move.IsPromotion)
-            //     should be a better piece         pawn
-            res += value(move.PromotionPieceType) - myPieceValue;
+        Move[] legalMoves = board.GetLegalMoves();
 
-        // prefer to move pawns
-        res += move.MovePieceType == PieceType.Pawn ? 1 : 0;
-        return res;
-    }
-
-    public Move Think(Board board, Timer timer)
-    {
-        Move[] moves = board.GetLegalMoves();
-        // PriorityQueue<Move, int> mvpq = new PriorityQueue<Move, int>();
-        Move chosenMove = moves[0];
-        foreach (Move move in moves)
+        // if we finished the search, evaluate the position
+        if (depth == 0 || legalMoves.Length == 0)
         {
-            if (eval(board, move) > eval(board, chosenMove))
-                chosenMove = move;
-            // int val = 10000;
-            
-            // if (isCheckmate(board, mv))
-            // {
-            //     return mv;
-            // }
-            // if (mv.IsCapture)
-            // {
-            //     Piece capturedPiece = board.GetPiece(mv.TargetSquare);
-            //     int capturedPieceValue = value(capturedPiece);
-            //     val = val - capturedPieceValue + pieceValues[(int)mv.CapturePieceType];
-            // }
-            //
-            // if (mv.IsPromotion)
-            // {
-            //     board.MakeMove(mv);
-            //     val -= value(board.GetPiece(mv.TargetSquare));
-            //     // pieceValues[(int)mv.PromotionPieceType];
-            //
-            // }
+            int eval = 0;
+            //  being checkmated is bad
+            if (board.IsInCheckmate())
+                return -1000_000_000;
+                    
+            if (board.IsInCheck())
+                // has to be at least 100, or black won't checkmate
+                eval += board.IsWhiteToMove ? -100 : 100;
 
+            // sum up the pieces from whites perspective
+            for (int i = 1; i < 7; ++i)
+                eval += board.GetPieceList((PieceType)i, true).Count * pieceValues[i];
 
-            // mvpq.Enqueue(move, val);
+            // sum up the pieces from blacks perspective
+            for (int i = 1; i < 7; ++i)
+                eval -= board.GetPieceList((PieceType)i, false).Count * pieceValues[i];
+
+            // if we're white, color will be 1 otherwise -1
+            return color * eval;
         }
 
+        //int bestEval = -Int32.MaxValue;
+        foreach (Move move in legalMoves)
+        {
+            board.MakeMove(move);
+            // decay added for further-out moves to stop prioritizing longer checkmates
+            int evaluation = (int) (0.99 * -negaMax(board, depth - 1, -beta, -alpha, -color));
+            board.UndoMove(move);
 
-        // return mvpq.Dequeue();
+            if (evaluation > bestEval)
+            {
+                bestEval = evaluation;
+                if (depth == maxDepth) chosenMove = move;
+            }
+            alpha = Math.Max(alpha, bestEval);
+            if (alpha >= beta) break;
+        }
+
+        hashes[hash % lookupTableSize] = hash;
+        values[hash % lookupTableSize] = bestEval;
+
+        return bestEval;
+    }
+    public Move Think(Board board, Timer timer)
+    {
+        Array.Clear(hashes);
+        Array.Clear(values);
+        int enemyPieceCount = 0;
+        int myPieceCount = 0;
+
+        foreach (var pieceList in board.GetAllPieceLists())
+        {
+            if (pieceList.IsWhitePieceList == board.IsWhiteToMove)
+                myPieceCount += pieceList.Count;
+            else
+                enemyPieceCount += pieceList.Count;
+        }
+        
+        chosenMove = board.GetLegalMoves()[0];
+
+        if (enemyPieceCount * 2 <= myPieceCount)
+            endGame = true;
+        else
+            endGame = false;
+        
+        // so far this seems to be fine
+        maxDepth = endGame ? 8 : 5;
+
+        negaMax(board, maxDepth, -Int32.MaxValue, Int32.MaxValue, board.IsWhiteToMove ? 1 : -1);
+        
+
         return chosenMove;
-    }
-
-
-    bool isCheck(Board board, Move move)
-    {
-        board.MakeMove(move);
-        bool isCheck = board.IsInCheck();
-        board.UndoMove(move);
-        return isCheck;
-    }
-    
-    bool isCheckmate(Board board, Move move)
-    {
-        board.MakeMove(move);
-        bool isCMate = board.IsInCheckmate();
-        board.UndoMove(move);
-        return isCMate;
     }
 }
